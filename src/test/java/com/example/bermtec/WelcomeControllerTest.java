dad8b105175ccd6133fb6d8fa38969e75fd37189
Satya <slgottipati@gmail.com>
2022-10-10 09:46:52 -0400

    updated

--- src/test/java/com/example/bermtec/WelcomeControllerTest.java
+++ src/test/java/com/example/bermtec/WelcomeControllerTest.java
@@ -23,7 +23,7 @@ public class WelcomeControllerTest {
     public void welcome() throws Exception {
 		  this.mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("welcome"))
+                .andExpect(view().name("welcome"));
              // .andExpect(content().string(containsString("welcome")));
     }
 }
